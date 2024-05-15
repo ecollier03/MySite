845da87864f7287a3196703ddfd613c2f18c57b5
Ethan Collier <ethanwaynecollier@gmail.com>
2024-05-15 15:23:04 -0600

    revamped file structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      Header and Accordion here
+    </div>
+  )
+}
+
+export default App;
